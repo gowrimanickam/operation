bb1ffb4d961f4f864d9a4da37dc9f3478201f624
gowrimanickam <46565775+gowrimanickam@users.noreply.github.com>
2019-01-31 18:48:33 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -5,8 +5,8 @@ char ch;
 printf("input \n");
 scanf("%c",&ch);
 if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
-printf("vowels");
+printf("Vowels");
 else 
-printf("consonant");
+printf("Consonant");
 return 0;
 }
