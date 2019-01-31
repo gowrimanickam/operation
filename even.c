1d203b05ce851da98a27841386f929b9d3eb69fb
gowrimanickam <46565775+gowrimanickam@users.noreply.github.com>
2019-01-31 18:32:52 +0530

    Update even.c

--- even.c
+++ even.c
@@ -2,13 +2,9 @@
 int main()
 {
 int n;
-printf("Enter a number \n");
-scanf("%d",&n);
 if(n%2==0)
-printf("Even number");
-else if(n==1)
-printf("odd number");
-  else
-    printf("invalid number");
+printf("Even");
+else 
+  printf("Odd");
 return 0;
 }
