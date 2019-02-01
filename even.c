2120bffe43a02f855637940bce436492d9433bfe
gowrimanickam <46565775+gowrimanickam@users.noreply.github.com>
2019-02-01 09:49:16 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int num;
-  scanf("%d",&num);
-  if(num>0)
+int n;
+  scanf("%d",&n);
+  if(n>0)
   {
-if(num%2==1)
+if(n%2==1)
 printf("Odd");
 else 
   printf("Even");
